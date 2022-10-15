f7144de5ce2ec210be3dd0e982d8f21a285fe891
Suruchi Daga <suruchibahety@gmail.com>
2022-10-15 20:52:11 +0530

    main page changes

--- src/containers/FamilyDetails/UpcomingEvents.js
+++ src/containers/FamilyDetails/UpcomingEvents.js
@@ -44,12 +44,24 @@ const UpcomingEvents = (props) => {
     return Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25)) + 1;
   };
 
+  const titleCase = (str) => {
+   if (typeof str !== 'undefined') {
+    return str
+        .split(' ')
+        .map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase())
+        .join(' ');
+    } else {
+        return "";
+    }
+}
+
   // Filtering the current date data
   const getBirthdayData = (date) => {
     const filtedData = props.familyData.filter(
       (obj) =>
         (getDate(obj.DOB).getDate() >= date.getDate() &&
-        getDate(obj.DOB).getMonth() === date.getMonth()) /*||
+        getDate(obj.DOB).getMonth() === date.getMonth() &&
+        obj.PunyaThitii == '') /*||
         (//new Date(obj.DOA).getDate() === date.getDate() &&
         new Date(obj.DOA).getMonth() === date.getMonth())*/
     );
@@ -91,12 +103,16 @@ const UpcomingEvents = (props) => {
       dataSource={birthdayData}
       renderItem={bObj => (
         <List.Item>
-          <Typography.Text mark>{bObj.NAME} turns {getAge(bObj.DOB)} Years Old on {getDateName(bObj.DOB)}</Typography.Text>
+          <Typography.Text><span style={{fontWeight:'bold'}}>{titleCase(bObj.NAME)}</span> turns {getAge(bObj.DOB)} on {getDateName(bObj.DOB)}....Say Hi
+          {(bObj.MOBILENO != '') && (
+          <span> @ <a href={'tel:'+ bObj.MOBILENO}>{bObj.MOBILENO}</a></span>
+          )}
+          </Typography.Text>
         </List.Item>
       )}
     />
     </Card>
-    
+
     <Card>
         <List
       header={<div>Upcoming Anniversaries</div>}
@@ -105,7 +121,7 @@ const UpcomingEvents = (props) => {
       dataSource={anniversaryData}
       renderItem={bObj => (
         <List.Item>
-          <Typography.Text mark>{bObj.NAME}\'s anniversary  on {bObj.DOA}</Typography.Text>
+          <Typography.Text><span style={{fontWeight:'bold'}}>{titleCase(bObj.NAME)} 's</span> anniversary  on {bObj.DOA}</Typography.Text>
         </List.Item>
       )}
     />
