f968baf8cfd5afc505e9e82cddd7e509b64e02d9
Suruchi Daga <suruchibahety@gmail.com>
2022-12-03 20:40:09 +0530

    prayer feature added

--- src/components/utility/supabase.js
+++ src/components/utility/supabase.js
@@ -3,9 +3,9 @@ const { createClient } = require("@supabase/supabase-js");
 //const url = "<SUPABASE_URL>";
 //const key = "<SUPABASE_KEY>";
 
-const url = "https://bnyaacwroakthbkyxsss.supabase.co";
+const url = "https://audpokvbjaytwtmspxjn.supabase.co";
 const key =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJueWFhY3dyb2FrdGhia3l4c3NzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgxOTE5MTAsImV4cCI6MTk4Mzc2NzkxMH0.9b_0eVf-liwfOBoTypuYgHQLEk8SnMGAgnm0lgU3diQ";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJueWFhY3dyb2FrdGhia3l4c3NzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgxOTE5MTAsImV";
 
 const supabase = createClient(url, key);
 
